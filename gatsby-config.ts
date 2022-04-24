d352fc226cfa45cde466b637cc5a87a5712a6ff3
Hoyeoun Byun <jay08111@naver.com>
2022-04-24 17:17:53 +0900

    # ADD - add tailwind css

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `myBlogForDev`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-emotion"]
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-postcss"],
 };
 
 export default config;
